0acf814c1fee70d37994a23da49de5bde6072c4b
SV-Martins <sabrinamartins2372@gmail.com>
2023-05-31 14:35:33 -0700

    task1

--- task1.c
+++ task1.c
@@ -3,11 +3,12 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
 
     int num_chars_printed = 0;
     const char *ptr = format;
+
+    va_list args;
+    va_start(args, format);
 
     while (*ptr != '\0')
     {
@@ -16,14 +17,14 @@ int _printf(const char *format, ...)
             ptr++;
             if (*ptr == 'c')
             {
-                // Print a single character
+                /* Print a single character*/
                 char c = (char)va_arg(args, int);
                 putchar(c);
                 num_chars_printed++;
             }
             else if (*ptr == 's')
             {
-                // Print a string
+                /* Print a string*/
                 const char *str = va_arg(args, const char *);
                 while (*str != '\0')
                 {
@@ -34,14 +35,14 @@ int _printf(const char *format, ...)
             }
             else if (*ptr == '%')
             {
-                // Print a literal '%'
+                /* Print a literal '%' */
                 putchar('%');
                 num_chars_printed++;
             }
         }
         else
         {
-            // Print any non-format specifier characters
+            /* Print any non-format specifier characters*/
             putchar(*ptr);
             num_chars_printed++;
         }
@@ -52,4 +53,12 @@ int _printf(const char *format, ...)
     va_end(args);
 
     return num_chars_printed;
+}
+int main()
+{
+    _printf("Hello, %s! Today is %s.\n", "James", "Wednesday");
+    _printf("The value of x is %d.\n", 42);
+    _printf("Printing a percent sign: %%\n");
+
+    return 0;
 }
