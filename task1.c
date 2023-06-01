cf9b1b3dda7fb92c5c8f674148f1ccbabb9baccb
amara-ani <amarachukwuani23@gmail.com>
2023-06-01 14:22:32 -0700

    fixed %d

--- task1.c
+++ task1.c
@@ -1,64 +1,61 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-
 int _printf(const char *format, ...)
 {
-
-    int num_chars_printed = 0;
-    const char *ptr = format;
-    
-    va_list args;
-    va_start(args, format);
-    
-    while (*ptr != '\0')
-    {
-	if (*ptr == '%')
+	int num_chars_printed = 0;
+	const char *ptr = format;
+	va_list args;
+	
+	va_start(args, format);
+	while (*ptr != '\0')
 	{
-    	ptr++;
-            if (*ptr == 'c')
-            {
-		/* Print a single character*/
-                char c = (char)va_arg(args, int);
-                putchar(c);
-                num_chars_printed++;
-            }
-            else if (*ptr == 's')
-            {
-                /* Print a string*/
-                const char *str = va_arg(args, const char *);
-                while (*str != '\0')
-                {
-                    putchar(*str);
-                    str++;
-                    num_chars_printed++;
-                }
-            }
-            else if (*ptr == '%')
-            {
-                /* Print a literal '%' */
-                putchar('%');
-                num_chars_printed++;
-            }
-	    else if (*ptr == 'd' || *ptr == 'i')
-            {
-                /* Print an integer*/
-                int value = va_arg(args, int);
-                printf("%d", value);
-                num_chars_printed += printf("%d", value);
-            }
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == 'c')
+			{
+				/* Print a single character*/
+				char c = (char)va_arg(args, int);
+
+				putchar(c);
+				num_chars_printed++;
+			}
+			else if (*ptr == 's')
+			{
+				/* Print a string*/
+				const char *str = va_arg(args, const char *);
+
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					num_chars_printed++;
+				}
+			}
+			else if (*ptr == '%')
+			{
+				/* Print a literal '%' */
+				putchar('%');
+				num_chars_printed++;
+			}
+			else if (*ptr == 'd' || *ptr == 'i')
+			{
+				/* Print an integer*/
+				int value = va_arg(args, int);
+
+				printf("%d", value);
+				num_chars_printed++;
+			}
+		}
+		else
+		{
+			/* Print any non-format specifier characters*/
+			putchar(*ptr);
+			num_chars_printed++;
+		}
+		ptr++;
 	}
-        else
-        {
-            /* Print any non-format specifier characters*/
-            putchar(*ptr);
-            num_chars_printed++;
-        }
-
-        ptr++;
-    }
-
-    va_end(args);
-
-    return num_chars_printed;
+	va_end(args);
+	return (num_chars_printed);
 }
