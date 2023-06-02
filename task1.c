de0df2ca753181423e6b455512bf6282518a2cde
SV-Martins <sabrinamartins2372@gmail.com>
2023-06-02 04:52:45 -0700

    17

--- task1.c
+++ task1.c
@@ -8,17 +8,17 @@
  */
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
-       	{
-		printf("Error: format string is NULL.\n");
-		return -1;
-	}
-
 	int num_chars_printed = 0;
 	const char *ptr = format;
 	va_list args;
 	va_start(args, format);
 	
+	 if (format == NULL)
+        {
+                printf("Error: format string is NULL.\n");
+                return -1;
+        }
+
 	while (*ptr != '\0')
 	{
 		if (*ptr == '%')
